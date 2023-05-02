b023ea1e6b0402403a57d203023fb574d2191ee5
Dmytro Oliinyk <oliynuk_dima@ukr.net>
2023-05-02 13:01:18 +0300

    Json file was created.

--- src/main/java/homework10/task2/testTask2.java
+++ src/main/java/homework10/task2/testTask2.java
@@ -1,9 +1,6 @@
 package homework10.task2;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Queue;
 
 public class testTask2 {
@@ -12,7 +9,14 @@ public class testTask2 {
 
         InputStream is = new FileInputStream("src/main/java/homework10/task2/file.txt");
 
-        Queue resultList = MyFileReader.readFromFile(is);
+        Queue resultList =MyFileReader.readFromFile(is);
+
+        FileWriter fr = new FileWriter("src/main/java/homework10/task2/user.json");
+        fr.write(JsonTransformator.transform(resultList));
+        fr.flush();
+        fr.close();
+
+
         System.out.println(JsonTransformator.transform(resultList));
 
     }
